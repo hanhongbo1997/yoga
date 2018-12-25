706504a8046f0968943c5a2c449a8b39dfdb5187
hanhongbo1997 <867132193@qq.com>
2018-12-25 15:37:08 +0800

    后台模板

---
+++ resources/views/admin/index/index.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.layout.layout')
+
+@section('content')
+我是一个主页
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('admin/index','admin\IndexController');
+Route::resource('admin/users','admin\UsersController');
